30d51b46db0fc6019b779f38b2566c0c435656fe
djy20020612 <djy20020612@noreply.gitcode.com>
2026-02-18 15:12:55 +0800

    102. 二叉树的层序遍历

---
+++ src/main/java/type/Traversal/levelOrder.java
@@ -0,0 +1,53 @@
+package main.java.type.Traversal;
+
+import com.sun.org.apache.xerces.internal.impl.xpath.regex.REUtil;
+import javafx.scene.transform.Rotate;
+import main.java.easy.postorderTraversal;
+import sun.reflect.generics.tree.Tree;
+
+import javax.swing.*;
+import java.nio.channels.NonWritableChannelException;
+import java.util.*;
+
+public class levelOrder {
+    public class TreeNode{
+        int val;
+        TreeNode left,right;
+        TreeNode(){}
+        TreeNode(int val){
+            this.val = val;
+            left = null;
+            right = null;
+        }
+        TreeNode(int val, TreeNode left, TreeNode right){
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    public static void main(String[] args) {
+
+    }
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> ans = new ArrayList<>();
+        if(root==null)
+            return ans;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while(!queue.isEmpty()){
+            int n = queue.size();
+            List<Integer> tmp = new ArrayList<>();
+            for(int i=0;i<n;i++){
+                TreeNode node = queue.poll();
+                tmp.add(node.val);
+                if(node.left!=null)
+                    queue.offer(node.left);
+                if(node.right!=null)
+                    queue.offer(node.right);
+            }
+            ans.add(tmp);
+        }
+        return ans;
+
+    }
+}
