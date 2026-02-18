f80a2bd1e1d74699f80985fd53b297f234c4f6a6
djy20020612 <djy20020612@noreply.gitcode.com>
2026-02-18 15:12:08 +0800

    94. 二叉树的中序遍历

---
+++ src/main/java/type/Traversal/inorderTraversal.java
@@ -0,0 +1,58 @@
+package main.java.type.Traversal;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
+public class inorderTraversal {
+    public class TreeNode{
+        int val;
+        TreeNode left,right;
+        TreeNode(){}
+        TreeNode(int val){
+            this.val = val;
+            left = null;
+            right = null;
+        }
+        TreeNode(int val,TreeNode left,TreeNode right){
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+    // 递归
+    public static List<Integer> inorderTraversal(TreeNode root){
+        List<Integer> list = new ArrayList<>();
+        inorder(list,root);
+        return list;
+    }
+
+    public static void inorder(List<Integer> list, TreeNode node){
+        if(node==null)return;
+        inorder(list,node.left);
+        list.add(node.val);
+        inorder(list,node.right);
+    }
+
+    // 迭代
+    public static List<Integer> inorderTraversal_1(TreeNode root){
+        List<Integer> list = new ArrayList<>();
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        while(root!=null||!stack.isEmpty()){
+            while(root!=null){
+                stack.push(root);
+                root = root.left;
+            }
+            TreeNode cur = stack.pop();
+            list.add(cur.val);
+            root = cur.right;
+        }
+        return list;
+
+    }
+}
