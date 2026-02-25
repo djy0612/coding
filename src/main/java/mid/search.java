4c4a6b082beba545484b4f25c12359753a107f6a
djy20020612 <djy20020612@noreply.gitcode.com>
2026-02-25 12:34:03 +0800

    33. 搜索旋转排序数组

---
+++ src/main/java/mid/search.java
@@ -0,0 +1,26 @@
+package main.java.mid;
+
+public class search {
+    public int search(int[] nums, int target) {
+        int n = nums.length;
+        int left = 0, right = n - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] == target)
+                return mid;
+            if (nums[mid] >= nums[left]) {//左有序
+                if (nums[mid] > target && target >= nums[left])
+                    right = mid - 1;
+                else {
+                    left = mid + 1;
+                }
+            } else {
+                if (nums[mid] < target && target <= nums[right])
+                    left = mid + 1;
+                else
+                    right = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
