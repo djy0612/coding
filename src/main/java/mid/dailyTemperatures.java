eac05558534177025309ef1758882c21e7112374
djy20020612 <djy20020612@noreply.gitcode.com>
2026-02-25 12:32:49 +0800

    739. 每日温度

---
+++ src/main/java/mid/dailyTemperatures.java
@@ -0,0 +1,21 @@
+package main.java.mid;
+
+import javafx.scene.control.Skin;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class dailyTemperatures {
+    public int[] dailyTemperatures(int[] temperatures){
+        int[] ans = new int[temperatures.length];
+        Deque<Integer> stack = new ArrayDeque<>();
+        for (int i = 0; i < temperatures.length; i++) {
+            while (!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]) {
+                int x = stack.pop();
+                ans[x] = i - x;
+            }
+            stack.push(i);
+        }
+        return ans;
+    }
+}
